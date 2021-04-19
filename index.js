9cf10dbf86b425a76d1a22f70b253984624af7b1
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2021-04-20 03:37:04 +0800

    Added Googlestrategy

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google').Strategy
+const keys = require('./config/keys')
+
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send({hi: 'there'})
-})
 
-app.listen(5000);
+// clientId: 614974199632-a48ecgoqlarva9vv8hg3f3ferqoipuvh.apps.googleusercontent.com
+// secret key: zu1z5vg8LQ0K2gg7t0hH_WKl
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
