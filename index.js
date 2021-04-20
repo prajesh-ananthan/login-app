0afc9767a6c498f1cb34b2927927caef476035ac
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2021-04-20 22:23:17 +0800

    Google OAuth completed

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const passport = require('passport')
-const GoogleStrategy = require('passport-google').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('./config/keys')
-
 
 const app = express();
 
-// clientId: 614974199632-a48ecgoqlarva9vv8hg3f3ferqoipuvh.apps.googleusercontent.com
-// secret key: zu1z5vg8LQ0K2gg7t0hH_WKl
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -18,6 +15,10 @@ passport.use(
   })
 );
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
