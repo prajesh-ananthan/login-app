fea72d7a2c6b12e034100e25c35a178f20ac9ba1
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2021-04-28 10:01:21 +0800

    Added cookie sessions

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const cookieSession = require('cookie-session');
 const keys = require('../config/keys')
 const mongoose = require('mongoose')
 const User = mongoose.model('users') // collection name
